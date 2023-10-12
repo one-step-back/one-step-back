4b134a8231866ac0ed76ffd61ed9999490469221
손성민 <alexzzanga7407@gmail.com>
2023-10-12 17:48:38 +0900

    컨트롤러 작업 시작전 pr

---
+++ src/main/java/com/app/onestepback/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.app.onestepback.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping("/")
+public class MainController {
+
+    // 메인 페이지
+    @GetMapping
+    public String goToMainForm() {
+        return "/main/main";
+    }
+}
