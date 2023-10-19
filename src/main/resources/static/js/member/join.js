182547996f12562662a048784de349eca965e799
손성민 <alexzzanga7407@gmail.com>
2023-10-19 21:11:04 +0900

    크라우드 펀딩 작업 개시

--- src/main/resources/static/js/member/join.js
+++ src/main/resources/static/js/member/join.js
@@ -1,73 +1,3 @@
-$("div#back").hide();
-$("div.info").hide();
-$("div.join").hide();
-let step = 1;
-
-// info
-const $infoInputs = $("div.info input[type='text']");
-const nameRegex = /^[가-힣|a-z|A-Z|]+$/;
-const specialCharacterRegex = /[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gim;
-const birthRegex = /^(19[0-9][0-9]|20\d{2}).?(0[0-9]|1[0-2]).?(0[1-9]|[1-2][0-9]|3[0-1])$/;
-const phoneRegex = /^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/
-let infoBlurMessages = ["이름을 입력하세요.", "생년월일을 입력하세요.", "휴대폰 번호를 입력하세요."];
-let infoRegexMessages = ["영문 혹은 한글로 2자~20자로 입력해주세요.", "생년월일을 확인하세요.", "휴대폰 번호를 확인하세요."];
-const $infoHelp = $("div.info p.help");
-let infoCheck;
-let infoCheckAll = [false, false, false, false, false];
-
-$infoInputs.eq(2).on("focus", function(){
-    $(this).val($(this).val().replaceAll("-", ""));
-});
-
-$infoInputs.eq(1).on("focus", function(){
-    $(this).val($(this).val().replaceAll(".", ""));
-});
-
-$infoInputs.on("blur", function(){
-    let i = $infoInputs.index($(this));
-    let value = $(this).val();
-
-
-    $(this).next().hide();
-    $(this).next().fadeIn(500);
-
-    if(!value){
-        $infoHelp.eq(i).text(infoBlurMessages[i]);
-        showHelp($(this), "error.png");
-        infoCheck = false;
-        infoCheckAll[i] = infoCheck;
-        return;
-    }
-
-    switch(i){
-        case 0:
-            infoCheck = value.length > 1 && value.length < 21 && nameRegex.test(value) && !specialCharacterRegex.test(value);
-            break;
-        case 1:
-            infoCheck = birthRegex.test(value)
-            if(infoCheck){
-                $(this).val(value.replace(/^(\d{4})(\d{2})(\d{2})$/, `$1.$2.$3`));
-            }
-            break;
-        case 2:
-            infoCheck = phoneRegex.test(value);
-            if(infoCheck){
-                $(this).val(value.replace(/^(\d{2,3})(\d{3,4})(\d{4})$/, `$1-$2-$3`));
-            }
-            break;
-    }
-
-    infoCheckAll[i] = infoCheck;
-
-    if(!infoCheck){
-        $infoHelp.eq(i).text(infoRegexMessages[i]);
-        showHelp($(this), "error.png");
-        return;
-    }
-
-    $infoHelp.eq(i).text("");
-    showHelp($(this), "pass.png");
-});
 
 function showHelp($input, fileName){
     $input.next().attr("src", "/images/" + fileName);
@@ -82,41 +12,20 @@ function showHelp($input, fileName){
     }
 }
 
-function goJoin(){
-    $infoInputs.trigger("blur");
-    if(infoCheckAll.filter(check => check).length != $infoInputs.length){
-        let modalMessage = "<span>모든 정보를 정확히 입력하셔야</span><span>다음 단계로 진행됩니다.</span>";
-        showWarnModal(modalMessage);
-        return;
-    }
-
-    $("div.basic-info-container dd").each(function(i){
-        $(this).text($infoInputs.eq(i).val());
-    });
-
-    step = 3;
-
-    $("div.info").hide();
-    $("div.join").show();
-
-    $([document.documentElement,document.body]).animate({
-        scrollTop:0
-    },300);
-}
-
 // join
-const $joinInputs = $("div.join input[type='text'], div.join input[type='password']");
-const idRegex = /^(?!(?:[0-9]+)$)([a-zA-Z]|[0-9a-zA-Z]){4,}$/;
+const $joinInputs = $("div.join input[id = 'email'], div.join input[id ='password'], div.join input[id ='nickname']");
+const nameRegex = /^[가-힣|a-z|A-Z|]+$/;
+const specialCharacterRegex = /[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gim;
 const passwordNumberRegex =/[0-9]/g;
 const passwordEnglishRegex = /[a-z]/ig;
 const passwordSpecialCharacterRegex = /[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi;
 const emailFirstRegex =  /[`~!@#$%^&*|\\\'\";:\/?]/;
 const emailLastRegex = /[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-let joinBlurMessages = ["아이디를 입력하세요.", "비밀번호를 입력하세요.", "비밀번호 확인을 위해 한번 더 입력하세요.", "이메일을 입력하세요."];
-let joinRegexMessages = ["영문 혹은 영문과 숫자를 조합하여 4자~20자로 입력해주세요.", "공백 제외 영어 및 숫자, 특수문자 모두 포함하여 10~20자로 입력해주세요.", "위 비밀번호와 일치하지 않습니다. 다시 입력해주세요.", "이메일 주소를 확인해주세요.", "이메일 주소를 확인해주세요."];
+let joinBlurMessages = ["이메일을 입력하세요.", "비밀번호를 입력하세요.", "닉네임을 입력하세요."];
+let joinRegexMessages = ["이메일 주소를 확인해주세요.", "공백 제외 영어 및 숫자, 특수문자 모두 포함하여 10~20자로 입력해주세요.", "영문 혹은 한글로 2자~20자로 입력해주세요."];
 const $joinHelp = $("div.join p.help");
 let joinCheck;
-let joinCheckAll = [false, false, false, false, false];
+let joinCheckAll = [false, false, false];
 
 $joinInputs.on("blur", function(){
     let i = $joinInputs.index($(this));
@@ -134,8 +43,8 @@ $joinInputs.on("blur", function(){
     }
 
     switch(i){
-        case 0: // 아이디
-            joinCheck = value.length > 3 && value.length < 21 && idRegex.test(value) && !specialCharacterRegex.test(value);
+        case 0: // 이메일
+            joinCheck = !emailFirstRegex.test(value);
             break;
         case 1: // 비밀번호
             let numberCheck = value.search(passwordNumberRegex);
@@ -148,16 +57,8 @@ $joinInputs.on("blur", function(){
 
             joinCheck = condition1 && condition2 && condition3;
             break;
-        case 2: // 비밀번호 확인
-            joinCheck = $joinInputs.eq(i-1).val() == value;
-            break;
-        case 3: // 이메일
-            joinCheck = !emailFirstRegex.test(value);
-            break;
-        case 4: // 도메인
-            var condition1 = emailLastRegex.test(value);
-            var condition2 = value.substring(value.indexOf(".") + 1).length > 1;
-            joinCheck = condition1 && condition2;
+        case 2: // 이름
+            joinCheck = value.length > 1 && value.length < 21 && nameRegex.test(value) && !specialCharacterRegex.test(value);
             break;
     }
 
@@ -183,7 +84,7 @@ $joinInputs.on("blur", function(){
                     $joinHelp.eq(i).css('color', 'red')
                     showHelp($joinInputs.eq(i), "error.png");
                 }else{
-                    $joinHelp.eq(i).text("멋진 아이디네요!");
+                    $joinHelp.eq(i).text("사용할 수 있는 이메일이에요!");
                     $joinHelp.eq(i).css('color', '#7eb347')
                     showHelp($joinInputs.eq(i), "pass.png");
                 }
@@ -219,23 +120,3 @@ function send(){
 
     document.join.submit();
 }
-
-$("div#back").click(function(){
-    step--;
-
-    switch(step){
-        case 1:
-            $("div.info").hide();
-            $("div.term").show();
-            $("div#back").hide();
-            break;
-        case 2:
-            $("div.term").hide();
-            $("div.join").hide();
-            $("div.info").show();
-            break;
-    }
-    $([document.documentElement,document.body]).animate({
-        scrollTop:0
-    },300);
-});
