4c9779394f4c5474443c5a1bfafa222566011742
조남호 <devnamho0910@gmail.com>
2023-10-31 08:53:25 +0900

    조남호 - 오류수정1

--- src/main/java/com/app/onestepback/mapper/ArtistPostMapper.java
+++ src/main/java/com/app/onestepback/mapper/ArtistPostMapper.java
@@ -12,19 +12,19 @@ import java.util.Optional;
 
 @Mapper
 public interface ArtistPostMapper {
-//    멤버아이디로 조회된 포스트의 갯수
+    //    멤버아이디로 조회된 포스트의 갯수
     public int selectCountOfPost(Long memberId);
 
-//    아티스트 홈페이지에서 사용될 작성시간 기준 3개의 포스트
+    //    아티스트 홈페이지에서 사용될 작성시간 기준 3개의 포스트
     public List<ArtistPostDTO> select3Posts(Long memberId);
 
-//    포스트 리스트 출력 (페이지네이션)
+    //    포스트 리스트 출력 (페이지네이션)
     public List<ArtistPostDTO> selectAll(Long memberId, Pagination pagination);
 
-//    포스트 작성
+    //    포스트 작성
     public void insertPost(ArtistPostDTO artistPostDTO);
 
-//    포스트 종류 추가(아티스트 포스트)
+    //    포스트 종류 추가(아티스트 포스트)
     public void insertArtistPost(Long postId);
 
 
