abd9e38365ef921cea0d5dd6b359aaa9f829e47d
손성민 <alexzzanga7407@gmail.com>
2023-11-02 19:06:44 +0900

    서버 수정

--- src/main/java/com/app/onestepback/controller/MyArtistController.java
+++ src/main/java/com/app/onestepback/controller/MyArtistController.java
@@ -1,9 +1,14 @@
 package com.app.onestepback.controller;
 
+import com.app.onestepback.domain.MemberVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @Slf4j
@@ -11,5 +16,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MyArtistController {
 
     @GetMapping("list")
-    public void goToListForm(){;}
+    public void goToListForm(HttpSession session, Model model){
+        MemberVO memberVO = (MemberVO) session.getAttribute("member");
+        model.addAttribute("memberId", memberVO.getId());
+    }
 }
