91f011a0302550bc94a02ab88e45e47044c817f2
조남호 <devnamho0910@gmail.com>
2023-11-03 17:50:58 +0900

    조남호 - 수정사항

---
+++ src/main/java/com/app/onestepback/config/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.app.onestepback.config;
+
+import com.app.onestepback.interceptor.LoginInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+    private final LoginInterceptor loginInterceptor;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/my-page/**", "/my-artist/**", "/my-library/**");
+    }
+}
